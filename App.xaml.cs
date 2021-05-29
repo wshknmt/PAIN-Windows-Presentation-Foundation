3db8bcf1d57246acb585e1d93622acee30144e38
Paweł Martyniuk <01143561@pw.edu.pl>
2021-05-29 10:53:53 +0200

    BooksView

--- App.xaml.cs
+++ App.xaml.cs
@@ -13,5 +13,13 @@ namespace PAIN_WPF
     /// </summary>
     public partial class App : Application
     {
+        public MVVM.IWindowService WindowService { get; } = new MVVM.WindowService();
+       // private BooksModel BooksModel { get; } = new BooksModel();
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            /*base.OnStartup(e);
+            PAIN_WPF.ViewModels.BooksViewModel booksViewModel = new PAIN_WPF.ViewModels.BooksViewModel(BooksModel);
+            WindowService.Show(booksViewModel);*/
+        }
     }
 }
--- Models/BooksModel.cs
+++ Models/BooksModel.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PAIN_WPF.Models
+namespace PAIN_WPF
 {
     public class BooksModel
     {
